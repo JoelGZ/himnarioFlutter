557450eb192b1c51f50f93799cb94ca1727484c6
Joel <innovate.appsjgz@gmail.com>
2020-05-12 18:40:25 -0600

    Search in Busqueda works. CoroDetail finished layout.

--- lib/coro_detail_route.dart
+++ lib/coro_detail_route.dart
@@ -1,10 +1,11 @@
 import 'dart:ffi';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:himnario/data/coro.dart';
 
-const double _paddingSubheadings = 8.0;
-const double _leftPaddingText = 12.0;
+const double _paddingSubheadings = 12.0;
+const double _leftPaddingText = 20.0;
 
 class CoroDetailRoute extends StatelessWidget {
   final Coro coro;
@@ -51,130 +52,167 @@ class CoroDetailRoute extends StatelessWidget {
     }
   }
 
-  Widget _buildCitaWidget() {
-    if (coro.cita != null) {
-      return Row(
-        children: <Widget>[
-          Text(
-            "Cita: ",
-            style: TextStyle(fontWeight: FontWeight.bold),
+  Widget _buildInformacionWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+      Container(
+        color: Colors.grey[500],
+        child: Padding(
+          padding: const EdgeInsets.all(_paddingSubheadings),
+          child: Text(
+            "Información General",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400),
           ),
-          Text(coro.cita),
-        ],
-      );
-    } else {
-      return null;
-    }
+        ),
+      ),
+      Padding(
+          padding: EdgeInsets.fromLTRB(_leftPaddingText, 12.0, 8.0, 12.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Tonalidad: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    selectTonalidad(coro.tonalidad),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Velocidad: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(selectVelocidad(coro.velocidad)),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Tiempo: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(coro.tiempo.toString()),
+                ],
+              )
+            ],
+          )),
+    ],);
   }
 
   Widget _buildHistoriaWidget() {
-    if (coro.historia != null) {
-      return Row(
-        children: <Widget>[
-          Text(
-            "Historia: ",
-            style: TextStyle(fontWeight: FontWeight.bold),
+    var showCita = coro.cita != null;
+    var showHistoria = coro.historia != null;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          color: Colors.grey[500],
+          child: Padding(
+            padding: const EdgeInsets.all(_paddingSubheadings),
+            child: Text(
+              "Historia",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400),
+            ),
           ),
-          Text(coro.historia),
-        ],
-      );
-    } else {
-      return null;
-    }
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(_leftPaddingText, 12.0, 8.0, 12.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Autor Letra: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(coro.autorLetra),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Autor Música: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(coro.autorMusica),
+                ],
+              ),
+              ///Conditional rendering of cita info
+              showCita ? Row(
+                children: <Widget>[
+                  Text(
+                    "Cita: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(coro.cita),
+                ],
+              ) : Container(height: 0, width: 0,),
+              ///Conditional rendering of historia info
+              showHistoria ? Row(
+                children: <Widget>[
+                  Text(
+                    "Historia: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(coro.historia),
+                ],
+              ) : Container(height: 0, width: 0,)
+            ],
+          ),
+        ),
+      ],);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Text(coro.nombre),
-          Container(
-            color: Colors.grey[500],
-            child: Padding(
-              padding: const EdgeInsets.all(_paddingSubheadings),
-              child: Text("Información"),
-            ),
-          ),
-          Padding(
-              padding: EdgeInsets.all(_leftPaddingText),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Text(
-                        "Tonalidad: ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        selectTonalidad(coro.tonalidad),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                        "Velocidad: ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(selectVelocidad(coro.velocidad)),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                        "Tiempo: ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(coro.tiempo.toString()),
-                    ],
-                  )
-                ],
-              )),
-          Container(
-            color: Colors.grey[500],
-            child: Padding(
-              padding: const EdgeInsets.all(_paddingSubheadings),
-              child: Text("Letra"),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(_leftPaddingText),
-            child: Text(coro.cuerpo),
-          ),
-          Container(
-            color: Colors.grey[500],
-            child: Padding(
-              padding: const EdgeInsets.all(_paddingSubheadings),
-              child: Text("Historia"),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(_leftPaddingText),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "Autor Letra: ",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(coro.autorLetra),
-                  ],
+    return Scaffold(
+      appBar: AppBar(),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  coro.nombre,
+                  style: TextStyle(fontSize: 22.0),
                 ),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "Autor Música: ",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(coro.autorMusica),
-                  ],
+              ),
+              _buildInformacionWidget(),
+              Container(
+                color: Colors.grey[500],
+                child: Padding(
+                  padding: const EdgeInsets.all(_paddingSubheadings),
+                  child: Text(
+                    "Letra",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w400),
+                  ),
                 ),
-                _buildCitaWidget(),
-                _buildHistoriaWidget(),
-              ],
-            ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(_leftPaddingText, 12.0, 8.0, 12.0),
+                child: Text(
+                  coro.cuerpo,
+                  style: TextStyle(fontSize: 14.0),
+                ),
+              ),
+              _buildHistoriaWidget(),
+            ]),
           )
         ],
       ),
